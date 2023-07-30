3ea89eb3b3be299dfea4adbee5486f9812195a1d
arsp <arspprashanthan@gmail.com>
2023-07-30 19:19:23 +0530

    Added avro auto generated files

---
+++ common/common-application/src/main/java/com/online/ordering/system/application/handler/ErrorDTO.java
@@ -0,0 +1,13 @@
+package com.online.ordering.system.application.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ErrorDTO {
+    private final String code;
+    private final String message;
+}
